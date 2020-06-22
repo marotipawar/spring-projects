9552b2c4175bd946dabe8b6c40a553cc1ed355fb
marotipawar <pawarmw@gmail.com>
2020-06-22 16:20:15 +0530

    Listed all vet list to vet index page. closes #14

--- petclinic-web/src/main/java/com/maroti/controller/VetController.java
+++ petclinic-web/src/main/java/com/maroti/controller/VetController.java
@@ -1,14 +1,23 @@
 package com.maroti.controller;
 
+import com.maroti.services.VetServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vet")
 @Controller
 public class VetController {
 
+    private final VetServices vetServices;
+
+    public VetController(VetServices vetServices) {
+        this.vetServices = vetServices;
+    }
+
     @RequestMapping({"", "/", "/index", "index.html"})
-    public String vetList()
+    public String vetList(Model model)
     {
+        model.addAttribute("vet", vetServices.findAll());
         return "vets/index";
     }
 }
